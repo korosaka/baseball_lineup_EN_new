196e71de648a4c55cb96eac1cd213663987b52a3
koro <koro.nn99@gmail.com>
2020-09-09 06:15:39 -0700

    code reformat

--- app/src/androidTest/java/com/websarva/wings/android/baseballstartinglineup/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/websarva/wings/android/baseballstartinglineup/ExampleInstrumentedTest.java
@@ -1,7 +1,8 @@
 package com.websarva.wings.android.baseballstartinglineup;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry ;
+
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
